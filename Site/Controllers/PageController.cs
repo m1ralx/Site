dd4ad30dcd620f5c08eb7006797e5658e51d6827
Mironov Alexey <mironov.alexey.fiit.urfu@gmail.com>
2015-09-23 21:38:06 +0500

    Add basic menu

---
+++ Site/Controllers/PageController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Site.Controllers
+{
+    public class PageController : Controller
+    {
+        //
+        // GET: /Page/
+        
+        public ActionResult AboutMe()
+        {
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+
+        public ActionResult Contacts()
+        {
+            return View();
+        }
+
+    }
+}
