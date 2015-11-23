0b37c8ad8b4acd2d0735fc525a67cbfd0b035dd0
Mironov Alexey <mironov.alexey.fiit.urfu@gmail.com>
2015-11-24 04:06:37 +0500

    base gallery

---
+++ Site/Scripts/gallery.js
@@ -0,0 +1,58 @@
+﻿"use strict";
+var gallerySize = 8;
+var displayedPhotos = 4;
+$(document).ready(function () {
+    var curLeft = 1;
+    var currentChoice = 0;
+    function toLeft() {
+        if (curLeft === 1) {
+            return;
+        }
+        curLeft--;
+        for (let i = 1; i <= gallerySize; i++) {
+            if (curLeft <= i && i < curLeft + displayedPhotos) {
+                $(".image-" + i).show();
+            } else {
+                $(".image-" + i).hide();
+            }
+        }
+    }
+    function toRight() {
+        if (curLeft === gallerySize - displayedPhotos)
+            return;
+        curLeft++;
+        for (let i = 1; i <= gallerySize; i++) {
+            if (curLeft <= i && i < curLeft + displayedPhotos) {
+                $(".image-" + i).show();
+            } else {
+                $(".image-" + i).hide();
+            }
+        }
+    };
+    $("#button-left").click(toRight);
+    $("#button-right").click(toLeft);
+    for (let i = 1; i <= 8; i++) {
+        $(".image-" + i).click(function () {
+            currentChoice = i;
+            $(".modal").css("display", "flex");
+            $(".modal-image > img").attr("src", $(".image-" + i + " img").attr("src"));
+        });
+    }
+    $(".left").click(function () {
+        if (currentChoice < 2)
+            return;
+        currentChoice--;
+        toLeft();
+        $(".modal-image > img").attr("src", $(".image-" + currentChoice + " img").attr("src"));
+    });
+    $(".right").click(function () {
+        if (currentChoice > 7)
+            return;
+        currentChoice++;
+        toRight();
+        $(".modal-image > img").attr("src", $(".image-" + currentChoice + " img").attr("src"));
+    });
+    $(".closing").click(function () {
+        $(".modal").hide();
+    });
+});
