dc37d24b311af4c74aaa25765e16f27af66b2f26
Mironov Alexey <mironov.alexey.fiit.urfu@gmail.com>
2015-11-24 21:15:54 +0500

    gallery update

--- Site/Scripts/gallery.js
+++ Site/Scripts/gallery.js
@@ -2,37 +2,39 @@
 var gallerySize = 8;
 var displayedPhotos = 4;
 $(document).ready(function () {
-    var curLeft = 1;
+    var curShift = 0;
     var currentChoice = 0;
+    function rotate(ar, size) {
+        return ar.slice(-size).concat(ar.slice(0, -size)); // size > 0 то вправо
+    }
+    function shiftPhotos(shift) {
+        var gallery = $(".gallery");
+        var photos = gallery.children('div[class^="image-"]');
+        photos.detach();
+        var photosArray = [];
+        for (let i = 0; i < gallerySize; i++)
+            photosArray.push(photos[i]);
+        photosArray = rotate(photosArray, shift);
+        for (let i = 0; i < gallerySize; i++) {
+            photos[i] = photosArray[i];
+        }
+        gallery.append(photos);
+        gallery.children().slice(3, 3 + displayedPhotos).each(function() {
+            $(this).show();
+        });
+        gallery.children().slice(3 + displayedPhotos, 3 + gallerySize).each(function () {
+            $(this).hide();
+        });
+    }
     function toLeft() {
-        if (curLeft === 1) {
-            $("#button-left").css("visibility", "hidden");
-            return;
-        }
-        $("#button-right").css("visibility", "visible");
-        curLeft--;
-        for (let i = 1; i <= gallerySize; i++) {
-            if (curLeft <= i && i < curLeft + displayedPhotos) {
-                $(".image-" + i).show();
-            } else {
-                $(".image-" + i).hide();
-            }
-        }
+        curShift--;
+        curShift %= gallerySize;
+        shiftPhotos(1);
     }
     function toRight() {
-        if (curLeft === gallerySize - displayedPhotos) {
-            $("#button-right").css("visibility", "hidden");
-            return;
-        }
-        $("#button-left").css("visibility", "visible");
-        curLeft++;
-        for (let i = 1; i <= gallerySize; i++) {
-            if (curLeft <= i && i < curLeft + displayedPhotos) {
-                $(".image-" + i).show();
-            } else {
-                $(".image-" + i).hide();
-            }
-        }
+        curShift++;
+        curShift %= gallerySize;
+        shiftPhotos(-1);
     };
 
     $("#button-left").click(toLeft);
@@ -51,20 +53,23 @@ $(document).ready(function () {
     }
 
     $(".left").click(function () {
-        if (currentChoice === 1)
-            return;
-        currentChoice--;
         toLeft();
+        currentChoice--;
+        currentChoice = currentChoice < 0 ? -currentChoice : currentChoice;
+        if (currentChoice === 0)
+            currentChoice = gallerySize;
+        currentChoice %= (gallerySize + 1);
         var img = new Image();
         $(".main-image").attr("src", "/Content/Images/loading.gif");
         img.onload = function () { $(".main-image").attr("src", img.src); }
         img.src = "/Content/Gallery/" + currentChoice + ".jpg";
     });
     $(".right").click(function () {
-        if (currentChoice > gallerySize-1)
-            return;
-        currentChoice++;
         toRight();
+        currentChoice++;
+        if (currentChoice === 0)
+            currentChoice = gallerySize;
+        currentChoice %= (gallerySize + 1);
         var img = new Image();
         $(".main-image").attr("src", "/Content/Images/loading.gif");
         img.onload = function () { $(".main-image").attr("src", img.src); }
